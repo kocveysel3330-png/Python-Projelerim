c2445a11a2acdcff27efadd7dc7fe149f15b6fcf
parfumcu <kocveysel3330@gmail.com>
2026-07-17 12:12:11 +0300

    "İlk Python proje denemesi

--- profil.py
+++ profil.py
@@ -4,3 +4,14 @@ hedef_ulke = "ABD"
 print("İsim:", isim)
 print("Yaş:", yas)
 print("Hedef Ülke:", hedef_ulke)
+                                                
+
+
+
+isim = "Veysel"
+soyad = "Koc"
+yas = 16
+hedef_ulke = "ABD"
+print("İsim:", isim)
+print("Yaş:", yas)
+print("Hedef Ülke:", hedef_ulke)
