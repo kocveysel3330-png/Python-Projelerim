f5891b9ecb9a7711ba7269ea6a1ff6145a882b25
parfumcu <kocveysel3330@gmail.com>
2026-07-17 12:05:57 +0300

    İlk Python proje denemesi

--- profil.py
+++ profil.py
@@ -1,6 +1,7 @@
-isim = "Veysel"
-yas = 16
-hedef_ulke = "ABD"
-print("İsim:", isim)
-print("Yaş:", yas)
-print("Hedef Ülke:", hedef_ulke)
+Name = "Veysel"
+surname = "Koc"
+age = 16
+goal_country = "ABD"
+print("Name:", Name)
+print("Age:", ege)
+print("Goal Country:",goal_country)
