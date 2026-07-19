c2557901cb05ca040299dcac699d8084dc4d20c7
parfumcu <kocveysel3330@gmail.com>
2026-07-19 11:58:32 +0300

    Daire Alan ve Çevre Hesaplama

--- profil.py
+++ profil.py
@@ -1,32 +1,4 @@
-<<<<<<< HEAD
-isim = "Veysel"
-yas = 16
-hedef_ulke = "ABD"
-print("İsim:", isim)
-print("Yaş:", yas)
-print("Hedef Ülke:", hedef_ulke)
-                                                
-
-
-
-isim = "Veysel"
-soyad = "Koc"
-yas = 16
-hedef_ulke = "ABD"
-print("İsim:", isim)
-print("Yaş:", yas)
-print("Hedef Ülke:", hedef_ulke)
-=======
-Name = "Veysel"
-surname = "Koc"
-age = 16
-goal_country = "ABD"
-print("Name:", Name)
-print("Age:", ege)
-print("Goal Country:",goal_country)
->>>>>>> f5891b9ecb9a7711ba7269ea6a1ff6145a882b25
-
-
+0
 
 
 
@@ -38,4 +10,4 @@ daire_alan = pi * (r ** 2)
 daire_cevre = 2 * pi * r
 
 print("Dairenin Alanı:", daire_alan)
-print("Dairenin Çevresi:", daire_cevre)
+print("Dairenin Çevresi:", daire_cevre) 0
