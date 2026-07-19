53ed87de8e0d2c083f4eabd459111f63e814da31
parfumcu <kocveysel3330@gmail.com>
2026-07-19 11:55:49 +0300

    Daire Alan ve Çevre Hesaplama

--- profil.py
+++ profil.py
@@ -25,3 +25,17 @@ print("Name:", Name)
 print("Age:", ege)
 print("Goal Country:",goal_country)
 >>>>>>> f5891b9ecb9a7711ba7269ea6a1ff6145a882b25
+
+
+
+
+
+
+pi = 3.14
+r = float(input("Yarı çap giriniz: "))
+
+daire_alan = pi * (r ** 2)
+daire_cevre = 2 * pi * r
+
+print("Dairenin Alanı:", daire_alan)
+print("Dairenin Çevresi:", daire_cevre)
